0f158ebaa90145e23a8f9b8fe6aa40b82b339d46
Rio Gunawan <79248654+Rio-Gunawan@users.noreply.github.com>
2025-03-21 03:17:27 +0000

    ESLint設定でセミコロンの警告レベルをエラーから警告に変更

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,7 +34,7 @@ export default [
                 "unix"
             ],
             "semi": [
-                "error",
+                "warn",
                 "always"
             ],
             "no-console": "warn",
